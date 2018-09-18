7000b2a2683092a20b74a66e224c6786639d2328
amdomanska <aga@resin.io>
2018-09-18 20:15:22 +0100

    refactor(GUI): Style Progress Button with styled-component
    Convert scss files for Progress Button using styled-component library
    
    Signed-off-by: amdomanska <aga@resin.io>

--- lib/gui/app/components/progress-button/progress-button.jsx
+++ lib/gui/app/components/progress-button/progress-button.jsx
@@ -19,26 +19,63 @@ const ReactDOM = require('react-dom')
 const propTypes = require('prop-types')
 const Color = require('color')
 
-const styled = require('styled-components').default
+const { default: styled, keyframes } = require('styled-components');
+
 const { Button, ProgressBar, Provider } = require('rendition')
 
 const { colors, consts } = require('./../../theme')
-const { StepButton } = require('./../../styled-components')
+const { StepButton, StepSelection } = require('./../../styled-components')
 
-const FlashProgressBar = styled(ProgressBar).attrs({
-  className: "flash-progress-bar"
-})`
+const progressButtonStripesBackgroundColor = '#' + Color(colors.primary.background).desaturate(0.05).rgbNumber().toString(16)
+const progressButtonStripesForegroundColor = '#' + Color(colors.primary.background).darken(0.18).desaturate(0.2).rgbNumber().toString(16)
+
+const ProgressButtonStripes = keyframes `
+  0% {
+    background-position: 0 0;
+  }
+
+  100% {
+    background-position: 20px 20px;
+  }
+`
+
+const FlashProgressBar = styled(ProgressBar)`
   > div {
     color: white !important;
     text-shadow: none !important;
   }
 
   width: 100%;
-  max-width: $btn-min-width;
+  max-width: ${consts.btnMaxWidth};
   margin: auto;
 
-  background: ${Color(colors.warning.background).darken(0.05).string()};
+  background: ${Color(colors.warning.background).darken(0.18).string()};
 `
+
+const FlashProgressBarValidating = styled(FlashProgressBar) `
+
+  // Notice that we add 0.01 to certain gradient stop positions.
+  // That workarounds a Chrome rendering issue where diagonal
+  // lines look spiky.
+  // See https://github.com/resin-io/etcher/issues/472
+
+  background-image: -webkit-gradient(linear, 0 0, 100% 100%,
+    color-stop(0.25, ${progressButtonStripesForegroundColor}),
+    color-stop(0.26, ${progressButtonStripesBackgroundColor}),
+    color-stop(0.50, ${progressButtonStripesBackgroundColor}),
+    color-stop(0.51, ${progressButtonStripesForegroundColor}),
+    color-stop(0.75, ${progressButtonStripesForegroundColor}),
+    color-stop(0.76 , ${progressButtonStripesBackgroundColor}),
+    to(${progressButtonStripesBackgroundColor}));
+
+  background-color: white;
+
+  animation: ${ProgressButtonStripes} 1s linear infinite;
+  overflow: hidden;
+
+  background-size: 20px 20px;
+`
+
 class ProgressButton extends React.Component {
 
   constructor (props) {
@@ -66,26 +103,30 @@ class ProgressButton extends React.Component {
       if (this.state.striped){
         return (
           <Provider>
-            <ProgressBar className={"flash-progress-bar-validating"}
-              primary
-              emphasized
-              value={this.props.percentage}
-            >
-              {this.props.label}
-            </ProgressBar>
+            <StepSelection>
+              <FlashProgressBarValidating
+                primary
+                emphasized
+                value={this.props.percentage}
+              >
+                {this.props.label}
+              </FlashProgressBarValidating>
+            </StepSelection>
           </Provider>
         )
       }
       else {
         return (
           <Provider>
-            <FlashProgressBar
-              warning
-              emphasized
-              value={this.props.percentage}
-            >
-              {this.props.label}
-            </FlashProgressBar>
+            <StepSelection>
+              <FlashProgressBar
+                warning
+                emphasized
+                value={this.props.percentage}
+              >
+                {this.props.label}
+              </FlashProgressBar>
+            </StepSelection>
           </Provider>
         )
       }
@@ -93,13 +134,15 @@ class ProgressButton extends React.Component {
     else {
       return (
         <Provider>
-          <StepButton
-            primary
-            onClick={this.handleClick}
-            disabled={this.state.disabled}
-          >
-            {this.props.label}
-          </StepButton>
+          <StepSelection>
+            <StepButton
+              primary
+              onClick={this.handleClick}
+              disabled={this.state.disabled}
+            >
+              {this.props.label}
+            </StepButton>
+          </StepSelection>
         </Provider>
       )
     }
